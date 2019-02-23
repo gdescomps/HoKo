9555323139bb4716bedbaf355d1feaa280c7daa9
gdescomps <46891683+gdescomps@users.noreply.github.com>
2019-02-23 19:26:46 +0100

    Démarrage avec la fenêtre principale maximisée

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     InterfacePrincipale interfacePrincipale;
-    interfacePrincipale.show();
+    interfacePrincipale.showMaximized();
     
     return a.exec();
 }
