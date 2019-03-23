65fafc458c0323813bcdf4f178ace80e0daf2057
gdescomps <46891683+gdescomps@users.noreply.github.com>
2019-03-23 10:50:40 +0100

    Création de la classe Controleur

---
+++ Controleur.h
@@ -0,0 +1,20 @@
+#ifndef CONTROLEUR_H
+#define CONTROLEUR_H
+
+#include "Interface/InterfacePrincipale.h"
+
+class Controleur {
+
+	private:
+		InterfacePrincipale* interface;
+
+	public:
+		Controleur(){
+			//interface =  new InterfacePrincipale;
+		}
+
+
+}
+
+
+#endif // CONTROLEUR_H
