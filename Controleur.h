affad13ad7a116d83335ec05cf2d3cb4f99580f4
gdescomps <46891683+gdescomps@users.noreply.github.com>
2019-03-24 18:48:05 +0100

    Documentation

--- Controleur.h
+++ Controleur.h
@@ -9,14 +9,31 @@ class InterfacePrincipale;
 class Controleur {
 
 	private:
+		/** Gestionnaire d'image de l'application */
 		GestionImage* gestionImage;
+
+		/** Gestionnaire de traitement de l'application */
 		GestionTraitement* gestionTraitement;
+
+		/** Interface principale de l'application */
 		InterfacePrincipale* interface;
 
 	public:
+		/**
+		* Contructeur du controleur, crée le gestionnaire d'image, de traitements et l'interface principale
+		*/
 		Controleur();
 
+		/*
+		* Retourne un pointeur vers l'interface principale
+		* \return InterfacePrincipale* pointant vers l'interface principale
+		*/
 		InterfacePrincipale* getInterface();
+
+		/**
+		* Retourne un pointeur vers le gestionnaire d'image
+		* \return GestionImage* pointant vers le gestionnaire d'image 
+		*/
 		GestionImage* getGestionImage();
 
 
